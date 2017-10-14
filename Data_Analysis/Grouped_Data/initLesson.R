89a4ffa8147368d00cfdad4cc732aa8179ea2cdf
Walter Garcia-Fontes <walter.garcia@upf.edu>
2017-10-14 08:28:59 +0200

    Added a less on Grouped Data for the Data Analysis folder

---
+++ Data_Analysis/Grouped_Data/initLesson.R
@@ -0,0 +1,4 @@
+# assign("gdental", actuar::gdental, envir=globalenv())
+frequency_table <- matrix(c("0-20",5,"20-40",10,"40-60",25,"60-80",10,"80-100",5),ncol=2,byrow=TRUE)
+colnames(frequency_table) <- c("Grades","Frequency")
+rownames(frequency_table) <- c("","","","","")

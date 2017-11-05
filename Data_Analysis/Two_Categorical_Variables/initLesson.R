563b614d9d84d0b20458255abcc20231b743adb2
Walter Garcia-Fontes <wgarcia@walter>
2017-11-05 09:54:03 +0100

    Translation of Two Categorical Variables from Catalan

---
+++ Data_Analysis/Two_Categorical_Variables/initLesson.R
@@ -0,0 +1,7 @@
+county <- c("Barcelonès","Barcelonès","Barcelonès","Barcelonès","Barcelonès","Barcelonès","Barcelonès","Barcelonès","Barcelonès","Barcelonès","Noguera","Noguera","Noguera","Noguera","Noguera","Noguera","Noguera","Noguera","Noguera","Noguera","Noguera","Noguera","Noguera","Noguera","L'Anoia","L'Anoia","L'Anoia","L'Anoia","L'Anoia","L'Anoia","L'Anoia","L'Anoia","Bages","Bages","Bages","Bages")
+transport <- ("Car","Car","Car","Train","Train","Train","Bus","Bus","Bus","Bus","Car","Car","Car","Train","Train","Train","Bus","Bus","Bus","Bus","Car","Car","Car","Bus","Car","Car","Car","Train","Train","Train","Bus","Bus","Car","Bus","Train","Bycicle")
+commuting <- data.frame(county,transport)
+fans <- matrix(c(4,2,0,1,5,1,0,1,6),ncol=3, byrow=TRUE)
+colnames(fans) <- c("Barcelona","Madrid","Sevilla")
+rownames(fans) <- c("Barça","Real Madrid","Betis")
+fans <- as.table(fans)

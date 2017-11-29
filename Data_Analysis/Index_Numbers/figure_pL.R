113054f8a33a13aaed67d2796ea9c4a42d158812
Walter Garcia-Fontes <wgarcia@walter>
2017-11-29 15:05:53 +0100

    Corrections

--- Data_Analysis/Index_Numbers/figure_pL.R
+++ Data_Analysis/Index_Numbers/figure_pL.R
@@ -1,2 +1,2 @@
-plot <- qplot(x=rownames(Dades),y=pL,xlab="Year",ylab="Laspeyres Price Index", main="Price Index Plot") + geom_line(aes(group=1))
+plot <- qplot(x=rownames(Data),y=pL,xlab="Year",ylab="Laspeyres Price Index", main="Price Index Plot") + geom_line(aes(group=1))
 print(plot)

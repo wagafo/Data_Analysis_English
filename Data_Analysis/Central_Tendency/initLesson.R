f8b25f0b3dbc223adf01ac513caa8f8751e2aa0f
Walter Garcia-Fontes <walter.garcia@upf.edu>
2020-10-04 19:45:28 +0200

    More corrections openintro

--- Data_Analysis/Central_Tendency/initLesson.R
+++ Data_Analysis/Central_Tendency/initLesson.R
@@ -1,2 +1,2 @@
   assign("cars", openintro::cars93, envir=globalenv())
-  assign("mpg.midsize", cars[cars$type=="midsize","mpgCity"], envir=globalenv())
+  assign("mpg.midsize", cars[cars93$type=="midsize","mpgCity"], envir=globalenv())

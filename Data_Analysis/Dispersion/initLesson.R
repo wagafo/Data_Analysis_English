f09be2b14fe553e178c9a2d07deb51eecaa285e6
wgarcia <walter.garcia@upf.edu>
2020-10-08 13:04:39 +0200

    Correction cars93

--- Data_Analysis/Dispersion/initLesson.R
+++ Data_Analysis/Dispersion/initLesson.R
@@ -1,2 +1,2 @@
 assign("cars", openintro::cars93, envir=globalenv())
-assign("mpg.midsize", cars[cars93$type=="midsize","mpg_City"], envir=globalenv())
+assign("mpg.midsize", cars[cars93$type=="midsize","mpg_city"], envir=globalenv())
